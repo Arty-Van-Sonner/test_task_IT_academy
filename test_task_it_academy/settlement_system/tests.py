d8fe2da59edeb53b90f595211602e788ffaf16ae
Artem Savchenko <valter.199669@gmail.com>
2025-09-25 14:06:04 +0300

    added swagger

--- test_task_it_academy/settlement_system/tests.py
+++ test_task_it_academy/settlement_system/tests.py
@@ -4,11 +4,11 @@ from rest_framework.test import APIClient
 from settlement_system.models import Wallet, Currency
 
 class SettlementSystemTest(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = APIClient()
         self.wallet = Wallet.objects.create(name='Test', currency=Currency.default_currency())
 
-    def test_deposit(self):
+    def test_deposit(self) -> None:
         url = reverse('wallet-operation', args=(self.wallet.uuid,))
         response = self.client.post(url, {'operation_type': 'DEPOSIT', 'amount': 1000}, format='json')
         self.wallet.refresh_from_db()
@@ -17,7 +17,7 @@ class SettlementSystemTest(TestCase):
         self.assertEqual(float(self.wallet.balance), 1000.0)
         self.assertEqual(response.data, {'wallet': str(self.wallet), 'balance': 1000})
 
-    def test_withdraw_success(self):
+    def test_withdraw_success(self) -> None:
         self.wallet.balance = 1000
         url = reverse('wallet-operation', args=(self.wallet.uuid,))
         response = self.client.post(url, {'operation_type': 'WITHDRAW', 'amount': 500}, format='json')
@@ -26,13 +26,13 @@ class SettlementSystemTest(TestCase):
         self.assertEqual(float(self.wallet.balance), 500.0)
         self.assertEqual(response.data, {'wallet': str(self.wallet), 'balance': 500})
 
-    def test_withdraw_insufficient_funds(self):
+    def test_withdraw_insufficient_funds(self) -> None:
         url = reverse('wallet-operation', args=(self.wallet.uuid,))
         response = self.client.post(url, {'operation_type': 'WITHDRAW', 'amount': 100}, format='json')
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.data, {'detail': 'There are insufficient funds in the account'})
 
-    def test_get_balance(self):
+    def test_get_balance(self) -> None:
         self.wallet.balance = 1000
         url = reverse('wallet-balance', args=(self.wallet.uuid,))
         response = self.client.get(url, format='json')
--- test_task_it_academy/settlement_system/views.py
+++ test_task_it_academy/settlement_system/views.py
@@ -1,13 +1,24 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.request import Request
 from .models import Wallet, Operation, Currency
 from .serializers import WalletSerializer, OperationSerializer
 from django.shortcuts import get_object_or_404
 from decimal import Decimal
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 
 class WalletCreateView(APIView):
-    def post(self, request, format=None, *args, **kwargs): 
+    @extend_schema(
+        summary='Create wallet',
+        description='Create a new wallet with the specified name',
+        request=WalletSerializer,
+        responses={
+            201: {'description': 'Successful',},
+            400: {'description': 'Bad requerst',},
+        } 
+    )
+    def post(self, request: Request, format=None, *args, **kwargs) -> Response: 
         serializer = WalletSerializer(data=request.data)
         if serializer.is_valid():
             new_wallet = serializer.save()
@@ -15,7 +26,42 @@ class WalletCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class WalletOperationView(APIView):
-    def post(self, request, wallet_uuid, format=None, *args, **kwargs):
+    @extend_schema(
+        summary='Performing an operation with a wallet',
+        description='Performs deposit (DEPOSIT) or withdrawal (WITHDRAW) of funds.',
+        request=OperationSerializer,
+        examples=[
+            OpenApiExample(
+                'Example of adding funds to an account',
+                summary='Deposit wallet',
+                description='Example of a request to replenish a wallet by 1000',
+                value={
+                    "operation_type": "DEPOSIT",
+                    "amount": 1000.0,
+                },
+                request_only=True,
+            ),
+            OpenApiExample(
+                'Example of withdrawal funds to an account',
+                summary='Withdrawal wallet',
+                description='Example of a withdrawal request from a 500 wallet',
+                value={
+                    "operation_type": "WITHDRAW",
+                    "amount": 500.0,
+                    "description": "Payment"
+                },
+                request_only=True,
+            ),
+        ],
+        responses={
+            201: {'description': 'Successful',},
+            400: {'description': 'Bad requerst',},
+            403: {'description': 'Insufficient funds',},
+            404: {'description': 'Wallet was not found',},
+        }
+    )
+    def post(self, request: Request, wallet_uuid: str, format=None, *args, **kwargs) -> Response:
+        print('\n\n', type(request), '\n\n')
         wallet = get_object_or_404(Wallet, uuid=wallet_uuid) 
         serializer = OperationSerializer(data=request.data)
         if serializer.is_valid():
@@ -32,7 +78,16 @@ class WalletOperationView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class GetWalletBalance(APIView):
-    def get(self, request, wallet_uuid, format=None, *args, **kwargs):
+    @extend_schema(
+        summary='Get the wallet balance',
+        description='Performs deposit (DEPOSIT) or withdrawal (WITHDRAW) of funds.',
+        request=OperationSerializer,
+        responses={
+            200: {'description': 'Successful',},
+            404: {'description': 'Wallet was not found',},
+        }
+    )
+    def get(self, request: Request, wallet_uuid: str, format=None, *args, **kwargs) -> Response:
         wallet = get_object_or_404(Wallet, uuid=wallet_uuid)
         data = {
             'wallet': str(wallet),
